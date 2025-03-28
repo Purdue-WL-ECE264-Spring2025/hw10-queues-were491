71bc9aedbeef54dbdb86e5895f36175157c667d1
Randolph Ha <rha051117@gmail.com>
2025-03-28 05:33:15 -0400

    cache prune

--- queue.c
+++ queue.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <stdlib.h>
 
+#define CACHE_SIZE 100000
+
 void enqueue(struct queue *q, struct game_state state)
 {
     assert(q != NULL);
@@ -19,7 +21,12 @@ struct game_state dequeue(struct queue *q)
 
 // Anything to prevent a time-out... well, while still being forced to use BFS
 // head is a dummy, first real element is head->next
-static void lln_push(struct list_node *head, size_t val) {
+static void lln_push(struct list_node *head, size_t val, size_t *cache) {
+  if (cache[(val >> 15) % CACHE_SIZE] == (val >> 15)) {
+    return;
+  }
+  cache[(val >> 15) % CACHE_SIZE] = val >> 15;
+
   struct list_node *first = malloc(sizeof(struct list_node));
   first->value = val;
   first->next = head->next;
@@ -62,13 +69,14 @@ static void lln_free(struct list_node *head) {
 int number_of_moves(struct game_state start)
 {
   // value of top 49 bits when game solved
-  const size_t solved = 81985526993846272;
+  const size_t solved = 81985526993846272 >> 15;
+  size_t *cache = calloc(CACHE_SIZE, sizeof(*cache));
 
   size_t cur_s;
   struct game_state cur;
 
   struct list_node *head = calloc(1, sizeof(struct list_node));
-  lln_push(head, serialize(start));
+  lln_push(head, serialize(start), cache);
 
   // actually this is just explosive so i guess the queue never empties
   errno = 0;
@@ -80,7 +88,7 @@ int number_of_moves(struct game_state start)
 
     cur = deserialize(cur_s);
     cur.num_steps &= (1 << 13) - 1;
-    if ((cur_s & ~((1 << 15) - 1)) == solved) {
+    if ((cur_s >> 15) == solved) {
       break;
     }
 
@@ -90,31 +98,32 @@ int number_of_moves(struct game_state start)
       start = cur;
       start.num_steps = cur.num_steps | (0b10 << 13);
       move_up(&start);
-      lln_push(head, serialize(start));
+      lln_push(head, serialize(start), cache);
     }
 
     if (cur.empty_row != 0 && ((cur_s >> 13) & 0b11) != 0b10) {
       start = cur;
       start.num_steps = cur.num_steps | (0b11 << 13);
       move_down(&start);
-      lln_push(head, serialize(start));
+      lln_push(head, serialize(start), cache);
     }
 
     if (cur.empty_col != 3 && ((cur_s >> 13) & 0b11) != 0b01) {
       start = cur;
       start.num_steps = cur.num_steps | (0b00 << 13);
       move_left(&start);
-      lln_push(head, serialize(start));
+      lln_push(head, serialize(start), cache);
     }
 
     if (cur.empty_col != 0 && ((cur_s >> 13) & 0b11) != 0b00) {
       start = cur;
       start.num_steps = cur.num_steps | (0b01 << 13);
       move_right(&start);
-      lln_push(head, serialize(start));
+      lln_push(head, serialize(start), cache);
     }
   }
 
+  free(cache);
   lln_free(head);
 
   if (errno == EINVAL) {
